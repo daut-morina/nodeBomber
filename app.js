3a146600513d6144374c5aa90d054de97b4e36e3
daut.morina <daut.morina>
2014-04-03 10:16:40 +0200

    make port heroku compatible

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(process.env.PORT || 3000);
 var io = require('socket.io').listen(server);
 
 var routes = require('./routes');
