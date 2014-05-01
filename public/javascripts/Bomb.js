9e46ec53d837e39a14b8e8ad82fecdd1c972cf3c
daut.morina <daut.morina>
2014-05-01 09:44:28 +0200

    bomb explosion

--- public/javascripts/Bomb.js
+++ public/javascripts/Bomb.js
@@ -1,6 +1,84 @@
 /**
  * Created by fabiolaib on 10.04.14.
  */
+
+var Bomb = function(initX, initY, bombContext) {
+    var id;
+    var x = initX;
+    var y = initY;
+
+    var detonating = false;
+    var detonated = false;
+
+    var context = bombContext;
+
+    var setId = function(newId) {
+        id = newId;
+    };
+
+    var getId = function() {
+        return id;
+    };
+
+    var setX = function(newX) {
+        x = newX;
+    };
+
+    var getX = function() {
+        return x;
+    };
+
+    var setY = function(newY) {
+        y = newY;
+    };
+
+    var getY = function() {
+        return y;
+    };
+
+    var isDetonated = function() {
+        return detonated;
+    };
+
+    var draw = function() {
+        if (!detonating) {
+            drawBomb();
+        } else {
+            drawExplosion();
+        }
+    };
+
+    var drawBomb = function() {
+        context.fillRect(x, y, 10, 10);
+    };
+
+    var drawExplosion = function() {
+        context.fillRect(x, y, 10, 60);
+        context.fillRect(x, y, 10, -50);
+        context.fillRect(x, y, 60, 10);
+        context.fillRect(x, y, -50, 10);
+    };
+
+    setTimeout(function() {
+        detonating = true;
+        setTimeout(function() {
+            detonated = true;
+        }, 1000);
+    }, 3000);
+
+    return {
+        setId:          setId,
+        getId:          getId,
+        setX:           setX,
+        getX:           getX,
+        setY:           setY,
+        getY:           getY,
+        isDetonated:    isDetonated,
+        draw:           draw
+    };
+};
+
+/*
 var Bomb = function(initX, initY, bombContext) {
     var id;
     var x = initX;
@@ -89,4 +167,4 @@ var Bomb = function(initX, initY, bombContext) {
         draw:       draw,
         drawExplosion: drawExplosion
     };
-};
+};*/
