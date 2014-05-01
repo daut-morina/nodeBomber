8deeacaa7f7ca8641c6eeb2bf4150522e670858d
c1honegg <c1honegg@hsr.ch>
2014-05-01 09:37:05 +0200

    add Inheritance from Item

--- public/javascripts/Bomb.js
+++ public/javascripts/Bomb.js
@@ -1,59 +1,23 @@
 /**
  * Created by fabiolaib on 10.04.14.
  */
-var Bomb = function(initX, initY) {
-    var id;
-    var x = initX;
-    var y = initY;
-    var detonate = false;
+function Bomb(initX, initY) {
+    console.log("bomb created");
+    Item.call(initX,initY, 10, "#ff0000");
+    this.detonate = false;
+    this.timer = setTimeout(this.boom, 3000);
+}
 
-    var boom = function() {
-        detonate = true;
+Bomb.prototype = {
+    boom: function() {
+        this.detonate = true;
+        console.log("boooooom");
+    },
+    getDetonate: function() {
+        return this.detonate;
     }
+}
+//Bomb.prototype.timer = setTimeout(this.boom, 3000);
+inherits(Bomb, Item);
 
-    var timer = setTimeout(boom, 3000);
 
-    var getDetonate = function() {
-        return detonate;
-    }
-    var setId = function(newId) {
-        id = newId;
-    }
-
-    var getId = function() {
-        return id;
-    };
-
-    var setX = function(newX) {
-        x = newX;
-    };
-
-    var getX = function() {
-        return x;
-    };
-
-    var setY = function(newY) {
-        y = newY;
-    };
-
-    var getY = function() {
-        return y;
-    };
-
-    var draw = function(context) {
-        context.fillRect(x, y, 10, 10);
-
-    };
-
-    return {
-        timer:       timer,
-        getDetonate: getDetonate,
-        setId:       setId,
-        getId:      getId,
-        setX:       setX,
-        getX:       getX,
-        setY:       setY,
-        getY:       getY,
-        draw:       draw
-    };
-};
